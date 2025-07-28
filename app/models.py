67bb2fd65f9ad3e073eede1f95a344e6c6a203d5
Subhangee <aradhya81702002@gmail.com>
2025-07-28 16:01:49 +0530

    Add files via upload

---
+++ app/models.py
@@ -0,0 +1,12 @@
+# models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class ImageUpload(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='uploads/')  # Adjust the directory as needed
+    predicted_class = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.predicted_class} - {self.timestamp}"
